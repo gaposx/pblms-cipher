dcfe9360981aad997511ee27d0c6c8bc10586604
Gonzalo Parra <gaposx@users.noreply.github.com>
2018-11-08 23:38:43 -0500

    Update cipher.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,43 +1,72 @@
+// objeto global window
 window.cipher = {
-  encode:function(offset, string){
-    console.log("Encode: " + offset + "|" + string);
-    var encodedString = "";
-    var asciiChar = "";
-    var moves = offset % 26;
-    
-    for (var i = 0; i < string.length; i++) {
-      asciiChar = string.charCodeAt(i);
-      if(asciiChar >= 65 && asciiChar <= 90) { //Mayusculas
-        asciiChar = ((asciiChar - 65 + moves) % 26) + 65;
+  // metodo encode
+  encode: (offset, string) => {
+    // convirtiendo offset a Interger
+    offset = parseInt(offset);
+    // creando una variable string para almacenamiento de las letras cifradas
+    let ciphertext = '';
+    // obteniendo el residuo del offset
+    offset = offset % 26;
+    // bucle para almacenar las letras cifradas
+    for (let i = 0 ; i < string.length ; i++) {
+      // convirtiendo a ASCII
+      let cAscii = string.charCodeAt(i);
+      let letter;
+      // diviendo el cifrado por letras mayusculas, minusculas y cuando no sea letra, no cifrar
+      if ((cAscii >= 65) && (cAscii <= 90)) {
+        letter = (cAscii - 65 + offset) % 26 + 65;
+      } else if ((cAscii >= 97) && (cAscii <= 122)) {
+        letter = (cAscii - 97 + offset) % 26 + 97;
+      } else {
+        letter = cAscii;
       }
-      if(asciiChar >= 97 && asciiChar <= 122) { //Minusculas
-        asciiChar = ((asciiChar - 97 + moves) % 26) + 97;
-      }
-      // if(asciiChar >= 48 && asciiChar <= 57) { //Numeros
-      //   moves = offset % 10;
-      //   asciiChar = ((asciiChar - 48 + moves) % 10) + 48;
-      // }
-      encodedString += String.fromCharCode(asciiChar);
+      // almacenando las letras cifradas y convirtiendolas en string
+      ciphertext += String.fromCharCode(letter);
     }
-    return encodedString;
-    
+    return ciphertext;
   },
-
-  decode:function(offset, string){
-    console.log("Decode: " + offset + "|" + string);
-    var new_offset = offset % 26;
-    var inverted_offset = 26 - new_offset;
-
-    var decodedString = window.cipher.encode(inverted_offset, string);
-    return(decodedString);
-
+  // metodo decode
+  decode: (offset, string) => {
+    // convirtiendo offset a Interger
+    offset = parseInt(offset);
+    // creando una variable string para almacenamiento de las letras cifradas
+    let ciphertext = '';
+    // obteniendo el residuo del offset
+    offset = offset % 26;
+    // bucle para almacenar las letras descifradas
+    for (let i = 0 ; i < string.length ; i++) {
+      // convirtiendo a ASCII
+      let cAscii = string.charCodeAt(i);
+      let letter;
+      // diviendo el descifrado por letras mayusculas, minusculas y cuando no sea letra, no descifrar
+      if ((cAscii >= 65) && (cAscii <= 90)) {
+        letter = (cAscii - 65 - offset + 26) % 26 + 65;
+      }else if ((cAscii >= 97) && (cAscii <= 122)) {
+        letter = (cAscii - 97 - offset + 26) % 26 + 97;
+      }else {
+        letter=cAscii;
+      }
+      // almacenando las letras descifradas y convirtiendolas en string
+      ciphertext += String.fromCharCode(letter);
+    }
+    return ciphertext;
   },
-
-  createCipherWithOffset:function(offset){
-    return { 
-			encode: (text) => cipher.encode(offset, text),
-			decode: (text) => cipher.decode(offset, text),
-		};
+  // metodo createCipherWithOffset
+  createCipherWithOffset: (offset) => {
+    // retornando un objeto con dos metodos
+    return {
+      // funcion encode
+      encode: (string) => {
+        // retornando cipher.encode con un string dado
+        return cipher.encode(offset, string);
+      },
+      // funcion decode
+      decode: (string) => {
+        // retornando cipher.decode con un string dado
+        return cipher.decode(offset, string);
+      }
+    }
   }
-  
 };
+  
